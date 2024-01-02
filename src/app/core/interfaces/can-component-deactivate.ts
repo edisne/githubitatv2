38d1d703847da92bea21107a8c774744fc8cf68a
Edis Neziric <e.neziric@3dqr.de>
2024-01-02 14:40:36 +0100

    App renamed

--- src/app/core/interfaces/can-component-deactivate.ts
+++
@@ -1,6 +0,0 @@
-import { Observable } from "rxjs";
-
-export interface CanComponentDeactivate {
-    hasUnsavedChanges: () => Observable <boolean> | Promise <boolean> | boolean;
-    isSubmited: boolean;
-}
